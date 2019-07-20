d90af75b926bec55fda77e1e061b3f3c50213983
stand01 <2507461149@qq.com>
2019-07-20 14:01:16 -0400

    clean cam4allin

--- Cam4_allin.py
+++ Cam4_allin.py
@@ -30,14 +30,14 @@ def Config_read():
             '10000'      : '',  # Finaff    Royal Cams  Done
             '10001'      : '',  # Adgaem    star stable Done
             '10002'      : '',  # Adpump    GETAROUND Done
-            '10003'      : '',  # UK Swipe 
-            '10004'      : 'http://ares.goldmaketing.com/c/10655/a?clickid=[clickid]&bid=[bid]&siteid=[siteid]&countrycode=[cc]&operatingsystem=[operatingsystem]&campaignid=[campaignid]&category=[category]&connection=[connection]&device=[device]&browser=[browser]&carrier=[carrier]',  # Ad1   opinion outpost   
+            '10003'      : '',  # UK Swipe  Done
+            '10004'      : '',  # Ad1   opinion outpost   Done
             '10005'      : '',  # Adsmain Cam4  Done           
-            '10006'      : 'https://track.adgaem.com/click?pid=1337&offer_id=62396',         # adgaem 1.2   GUARDIANS OF AMBER         
-            '10007'      : 'http://affprofile.o18.click/c?o=676208&m=1659&a=37792',         # Stripchat affprofile
+            '10006'      : '',         # adgaem 1.2   GUARDIANS OF AMBER         
+            '10007'      : '',         # Stripchat affprofile
             '10008'      : '',         # IKARIAM
             '10009'      : '',         # IKARIAM  
-            '10010'      : '',         # rifu Done
+            '10010'      : '',         # rifu Done chajian
             '10011'      : '',         # IKARIAM
             '10012'      : '',         # IKARIAM
             '10013'      : '',         # IKARIAM             '10006'      : '',         # LP            
