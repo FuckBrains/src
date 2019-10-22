4837d891b3f012526a85bf605f5f4343cd4f59c0
zhejiang <zhejiang@qq.com>
2019-10-23 03:52:54 +0800

    aaa

--- luminati_main.py
+++ luminati_main.py
@@ -1,6 +1,4 @@
 import threading
-import xlrd
-from xlutils.copy import copy
 import random
 import os
 import sys
@@ -9,140 +7,15 @@ import importlib
 import threadpool
 from time import sleep
 import Changer_windows_info as changer
-import imap_test
-import Hotmail_check as hotmail
-import psutil
-import ip_test
-from xlrd import xldate_as_tuple
-from urllib import request, parse
-import pymysql
 import db
-import re
-import json
-import luminati
 import tools
-import Chrome_driver
+import thread_tokill as tk
 
 '''
 'testeeeee'
 '''
 
-Falg_threads = 0
-Mission_num = 0
-
-
-
 pool = threadpool.ThreadPool(5)
-
-
-                 
-def multi_reg(Config):  
-    # print(Config)
-    global Falg_threads    
-    return_rand = random.randint(0,5)
-    if return_rand == 0:
-        print('unique  random,return for Mission_Id:',Config)
-    else:
-        time_cheat = random.randint(0,10)
-        print(Config)
-        if Config['Alliance'] != 'Test':
-            print('Sleep for random time:',time_cheat*60,'-------------')   
-            if Config['Mission_Id'] != '20000':
-                sleep(time_cheat*60)
-        else:
-            print('test...........')
-        while True:
-            print('getting data')
-            try:
-                submit = db.get_luminati_submit(Config)
-                print(submit)
-                # return
-                if Config['Alliance'] == 'Test':
-                    submit['state_'] = ''
-                # print('Data for this mission:')
-                # print(submit)
-            except Exception as e:
-                print(str(e))
-                Falg_threads += 1
-                print('Get data wrong..................................')
-                print('Falg_threads',Falg_threads)
-                print('Mission_num:',Mission_num)                
-                # changer.Restart()
-                return
-            if submit['Excels_dup'][1] != '':
-                print('testing email.........')
-                flag = imap_test.Email_emu_getlink(submit['Email'])
-                if flag == 0:
-                    print('Bad email:',submit['Email']['Email_emu'])
-                    db.updata_email_status(submit['Email']['Email_Id'],0)
-                    continue
-                elif flag == 1:
-                    print("Good email")
-                    db.updata_email_status(submit['Email']['Email_Id'],1)
-                else:
-                    print('Loging email server failed,find another email')
-                    continue
-            else:
-                pass 
-            print('refreshing ip.............')          
-            flag = luminati.ip_test(submit['port_lpm'],state=submit['state_'] ,country='')
-            print(flag,'=========================')
-            if flag == 1:
-                break
-            elif flag == -1:
-                print('bad port,change into new')
-                luminati.delete_port_s(submit['port_lpm'])            
-                port_new = luminati.get_port_random()
-                db.update_port(submit['port_lpm'],port_new)
-                Config['port_lpm'] = port_new
-                print(port_new)
-                try:
-                    luminati.add_proxy(port_new,country=submit['Country'],proxy_config_name='jia1',ip_lpm=submit['ip_lpm'])
-                except Exception as e:
-                    print(str(e))
-                continue
-            else:
-                continue
-            print('Reading config from sql server success')
-        module = 'Mission_'+str(submit['Mission_Id'])
-        Module = importlib.import_module(module)
-        flag = 0
-        try:
-            print('----------------====================')
-            chrome_driver = Chrome_driver.get_chrome(submit)
-            flag = Module.web_submit(submit,chrome_driver=chrome_driver)
-            print(submit)
-        except Exception as e:
-            print(str(e))
-        try:
-            chrome_driver.close()
-            chrome_driver.quit()
-        except:
-            pass
-        if flag == 1:
-            mission_check = {}
-            try:
-                mission_check = db.check_mission_status(submit)
-            except:
-                pass
-            if len(mission_check) == 0:
-                print('Mission: ',submit['Mission_Id'],'success,uploading db')
-                db.write_one_info([str(submit['Mission_Id'])],submit)
-        print('Mission_Id:',submit['Mission_Id'],'finished')
-    Falg_threads += 1  
-    print('Falg_threads',Falg_threads)
-    print('Mission_num:',Mission_num)
-    if Falg_threads == Mission_num:
-        try:
-            print('try killing pids')
-            tools.killpid()
-            print('kill pids finished')
-        except Exception as e:
-            print(str(e))
-            pass  
-    return  
-
-    # print('Falg_threads:',Falg_threads)
 
 def get_modules():
     modules = os.listdir('..\src\\')
@@ -190,15 +63,18 @@ def main(i):
         if len(plans) == 0:
             print('No plan for this computer!!!!!!')
             return
-        print('Missions:')
         # print(plans)
-        global Mission_num
-        Mission_num = len(plans)
-        print(Mission_num)
-        requests = threadpool.makeRequests(multi_reg, plans)
+        requests = threadpool.makeRequests(tk.multi_reg, plans)
         [pool.putRequest(req) for req in requests]
         pool.wait() 
         print('All Missions finished..............')
+        try:
+            print('try killing pids')
+            tools.killpid()
+            print('kill pids finished')
+        except Exception as e:
+            print(str(e))
+            pass          
         if i == 1:
             restart_time = random.randint(3,5)
             print('Mission completed.........')
@@ -213,7 +89,7 @@ def test():
     print(Config)
 
 if __name__ == '__main__':
-    paras=sys.argv
-    i = int(paras[1])
-    # i = 1
+    # paras=sys.argv
+    # i = int(paras[1])
+    i = 1
     main(i)
---
+++ thread_tokill.py
@@ -0,0 +1,104 @@
+import random
+from time import sleep
+import db
+import Chrome_driver
+import luminati
+import imap_test
+import importlib
+from wrapt_timeout_decorator import *
+
+
+@timeout(600)
+def multi_reg(Config):  
+    # print(Config)
+    return_rand = random.randint(0,5)
+    if return_rand == 0:
+        print('unique  random,return for Mission_Id:',Config)
+    else:
+        time_cheat = random.randint(0,10)
+        print(Config)
+        if Config['Alliance'] != 'Test':
+            print('Sleep for random time:',time_cheat*60,'-------------')   
+            # if Config['Mission_Id'] != '20000':
+            #     sleep(time_cheat*60)
+        else:
+            print('test...........')
+        while True:
+            print('getting data')
+            try:
+                submit = db.get_luminati_submit(Config)
+                print(submit)
+                # return
+                if Config['Alliance'] == 'Test':
+                    submit['state_'] = ''
+                # print('Data for this mission:')
+                # print(submit)
+            except Exception as e:
+                print(str(e))
+                Falg_threads += 1
+                print('Get data wrong..................................')
+                print('Falg_threads',Falg_threads)
+                print('Mission_num:',Mission_num)                
+                # changer.Restart()
+                return
+            if submit['Excels_dup'][1] != '':
+                print('testing email.........')
+                flag = imap_test.Email_emu_getlink(submit['Email'])
+                if flag == 0:
+                    print('Bad email:',submit['Email']['Email_emu'])
+                    db.updata_email_status(submit['Email']['Email_Id'],0)
+                    continue
+                elif flag == 1:
+                    print("Good email")
+                    db.updata_email_status(submit['Email']['Email_Id'],1)
+                else:
+                    print('Loging email server failed,find another email')
+                    continue
+            else:
+                pass 
+            print('refreshing ip.............')          
+            flag = luminati.ip_test(submit['port_lpm'],state=submit['state_'] ,country='')
+            print(flag,'=========================')
+            if flag == 1:
+                break
+            elif flag == -1:
+                print('bad port,change into new')
+                luminati.delete_port_s(submit['port_lpm'])            
+                port_new = luminati.get_port_random()
+                db.update_port(submit['port_lpm'],port_new)
+                Config['port_lpm'] = port_new
+                print(port_new)
+                try:
+                    luminati.add_proxy(port_new,country=submit['Country'],proxy_config_name='jia1',ip_lpm=submit['ip_lpm'])
+                except Exception as e:
+                    print(str(e))
+                continue
+            else:
+                continue
+            print('Reading config from sql server success')
+        module = 'Mission_'+str(submit['Mission_Id'])
+        Module = importlib.import_module(module)
+        flag = 0
+        try:
+            print('----------------====================')
+            chrome_driver = Chrome_driver.get_chrome(submit)
+            flag = Module.web_submit(submit,chrome_driver=chrome_driver)
+            print(submit)
+        except Exception as e:
+            print(str(e))
+        try:
+            chrome_driver.close()
+            chrome_driver.quit()
+        except:
+            pass
+        if flag == 1:
+            mission_check = {}
+            try:
+                mission_check = db.check_mission_status(submit)
+            except:
+                pass
+            if len(mission_check) == 0:
+                print('Mission: ',submit['Mission_Id'],'success,uploading db')
+                db.write_one_info([str(submit['Mission_Id'])],submit)
+        print('Mission_Id:',submit['Mission_Id'],'finished')
+    return  
