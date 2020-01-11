0f0e064e1ef0bef2c4d917d51ee78b9d40f9d650
zhejiang <zhejiang@qq.com>
2020-01-11 13:41:52 +0800

    'aaa'

--- Chrome_driver.py
+++ Chrome_driver.py
@@ -135,6 +135,7 @@ def get_chrome(submit = None,pic=0):
         uas = get_ua_all()
         ua = get_ua_random(uas)
         print(ua)
+        ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
     else:
         if 'traffic' in submit:
             options.add_argument('--headless')            
