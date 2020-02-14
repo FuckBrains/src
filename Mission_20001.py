274fe0d1df84d706480ce88b1a02dd06912e0c72
zhejiang <zhejiang@qq.com>
2020-02-13 23:49:45 -0500

    'aaa'

--- Mission_20001.py
+++ Mission_20001.py
@@ -37,19 +37,21 @@ def web_submit(submit,chrome_driver,debug=0):
 def test():
     # db.email_test()
     # date_of_birth = Submit_handle.get_auto_birthday('')         
-    Mission_list = ['10023']
-    excel = 'Uspd'    
+    Mission_list = ['10000']
+    excel = 'Uspd_big'  
+    # excel = 'Uspd'  
     Excel_name = [excel,'']
     Email_list = ['hotmail.com','outlook.com','yahoo.com','aol.com','gmail.com']
     submit = db.read_one_excel(Mission_list,Excel_name,Email_list)
-    # [print(item,':',submit[excel][item]) for item in submit[excel] if submit[excel][item]!=None]
-    [print(item,':',submit[excel][item]) for item in submit[excel] if item == 'homephone']  
+    # print(submit)
+    [print(item,':',submit[excel][item]) for item in submit[excel] if submit[excel][item]!=None]
+    # [print(item,':',submit[excel][item]) for item in submit[excel]]  
     submit['Mission_Id'] = '10023'
     # phone = submit[excel]['homephone']
     # phone = Submit_handle.get_uk_phone1(phone)
     # print(phone)
-    chrome_driver = Chrome_driver.get_chrome(submit)
-    web_submit(submit,chrome_driver,1)
+    # chrome_driver = Chrome_driver.get_chrome(submit)
+    # web_submit(submit,chrome_driver,1)
 
 
 if __name__=='__main__':
