71030ba717623af9a2d161e22bf15b20fa49eb43
stand01 <2507461149@qq.com>
2020-06-13 10:55:03 -0400

    'aaa'

--- Chrome_driver.py
+++ Chrome_driver.py
@@ -129,7 +129,7 @@ def get_chrome(submit = None,pic=0,headless=0,time_out=300):
     print('++++++++++++++++++++++++')
     print('++++++++++++++++++++++++')
     print('++++++++++++++++++++++++')
-    print('Chrome_driver file version : 1.1')
+    print('Chrome_driver file version : 1.2')
     # print([key for key in submit])
     # print('++++++++++++++++++++++++')
     # print('++++++++++++++++++++++++')
