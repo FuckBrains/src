99fbd7c4557938baa963836f5601f331f664844c
zhejiang <zhejiang@qq.com>
2020-07-24 15:06:48 +0800

    'aaa'

--- De_Collector.py
+++ De_Collector.py
@@ -1,45 +1,60 @@
 import json
 import sys
 sys.path.append("..")
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow , QApplication,QWidget, QMessageBox
+from PyQt5.QtWidgets import QMainWindow , QApplication
+from PyQt5.QtCore import pyqtSlot,QThread,QCoreApplication
 import os
-# os.system(r'pyuic5 -o uiclass2.py ui\de_collector.ui')
+# # os.system(r'pyuic5 -o uiclass2.py ui\de_collector.ui')
 from uiclass2 import Ui_MainWindow
-from PyQt5.QtCore import pyqtSlot
-
-
+from de_gen_excel import Collector
 
 
 class Mywindow(QMainWindow,Ui_MainWindow):
     def __init__(self,parent = None):
         QMainWindow.__init__(self,parent)
         self.setupUi(self) 
-        _translate = QtCore.QCoreApplication.translate
+        _translate = QCoreApplication.translate
 
     @pyqtSlot()
     def on_pushButton1_clicked(self):
         param = self.comboBox.currentText()
-        print('start with %s'%param)
+        self.thread = RunThread(1,param)
+        self.thread.start()        
+        # print('start with %s'%param)
+        # collector = Collector(1,param)   
+        # collector.final_yellow_page(QApplication)
+        # QApplication.processEvents()
         # db.init()
         # command = '''start cmd /k "python de_gen.pyc %d %s "{$name$:$qcy$}" && exit"'''%(1,param)
         # os.system(command)        
-        command = '''start cmd /k "python de_gen_excel.pyc %d %s "{$name$:$qcy$}" && exit"'''%(1,param)
-        os.system(command)        
+        # command = '''start cmd /k "python de_gen_excel.pyc %d %s "{$name$:$qcy$}" && exit"'''%(1,param)
+        # os.system(command)        
 
     @pyqtSlot()
     def on_pushButton2_clicked(self):
         # param = self.comboBox.currentText()
-        print('start sort')
-        # db.init()
-        command = '''start cmd /k "python de_gen_excel.pyc %d "{$name$:$qcy$}" && exit"'''%(0)
-        os.system(command)    
+        print('Collect data')
+        # QApplication.processEvents()
+        self.thread = RunThread(0,'')
+        self.thread.start()
 
+
+class RunThread(QThread):
+    def __init__(self,count,param):
+        super().__init__()
+        self.count = count
+        self.collector = Collector(count,param)
+
+    def run(self):
+        if self.count == 0:
+            self.collector.collect()
+        else:
+           self.collector.final_yellow_page()
 
 def main():
     # up.main()
     import traceback
-    app = QtWidgets.QApplication(sys.argv)  # 创建一个QApplication，也就是你要开发的软件app
+    app = QApplication(sys.argv)  # 创建一个QApplication，也就是你要开发的软件app
     MainWindow = QMainWindow()    # 创建一个QMainWindow，用来装载你需要的各种组件、控件
     # MainWindow.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint)
     # MainWindow.setFixedSize(MainWindow.width(), MainWindow.height());  
